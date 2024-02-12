7b97637dcb7edbcb76368eea132fe35364aa8ca6
OllyLicoly <naina_kievna@mail.ru>
2024-02-12 18:00:02 +0700

    placed static to method

--- src/main/java/web/dao/CarDaoImp.java
+++ src/main/java/web/dao/CarDaoImp.java
@@ -11,26 +11,21 @@ public class CarDaoImp implements CarDao{
 
     private List<Car> cars;
 
-    {
+    @Override
+    public List<Car> getAllCars() {
         cars = new ArrayList<>();
-
         cars.add(new Car("BMW", 5, "black"));
         cars.add(new Car("Audi", 8, "silver"));
         cars.add(new Car("Mercedes", 6, "red"));
         cars.add(new Car("Mazda", 3, "royal blue"));
         cars.add(new Car("Porsche", 7, "yellow"));
-    }
-
-
-    @Override
-    public List<Car> getAllCars() {
         return cars;
     }
 
 
     @Override
     public List<Car> getSomeCars(int count) {
-        return cars.subList(0, count);
+        return getAllCars().subList(0, count);
     }
 
 }
