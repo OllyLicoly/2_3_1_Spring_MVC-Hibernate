d2b38adb649a4346d20d116ae8cc95191d242f72
OllyLicoly <naina_kievna@mail.ru>
2024-02-19 12:54:34 +0700

    all CRUD functions app

---
+++ src/main/java/web/controller/UsersController.java
@@ -0,0 +1,72 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
+import org.springframework.ui.ModelMap;
+
+@Controller
+@RequestMapping(value = "/users")
+public class UsersController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public String printUsers(ModelMap model) {
+        model.addAttribute("users", userService.getAllUsers());
+
+        return "users";
+    }
+
+    @GetMapping("/delete")
+    public String deleteUser(@RequestParam("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/users";
+    }
+
+//    @GetMapping("/add_user_info")
+//    public String addUserInfo(@ModelAttribute("user") User user){
+//        User user = new User();
+//        model.addAttribute("user", user);
+//        return "userinfo";
+//    }
+//    @RequestMapping("/saveUser")
+//    public Model save(@RequestParam long id){
+//        Model model = new Model("save_user");
+//        userService.save(user);
+//        return "redirect:/";
+//    }
+
+    @GetMapping("/update")
+    public String updateUser(@RequestParam("id") Long id, Model model ){
+        User user = userService.getUser(id);
+        model.addAttribute("user", user);
+        return "userupdate";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/add")
+    public String addUser(@ModelAttribute("user") User user ){
+        return "useradd";
+    }
+
+    @PostMapping("/add")
+    public String addNewUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/users";
+    }
+
+
+
+
+
+}
